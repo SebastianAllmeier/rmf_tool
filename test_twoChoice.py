b1147a28a54fca02a691dee4e272bdf32d2dc20d
Nicolas Gast <nicolas.gast@inria.fr>
2020-09-09 22:32:33 +0200

    cosmetic changes to the file 'test_twoChoice'

--- test_twoChoice.py
+++ test_twoChoice.py
@@ -1,6 +1,11 @@
-import src.rmf_tool as rmf
-import numpy as np
+"""
+Test that computes the refined mean field approximation for the two-choice model
+(with order 1 and 2 and a few parameter)
+Compare the computed value with a value already stored in a pickle file
+"""
 import pickle
+import numpy as np
+import src.rmf_tool as rmf
 
 def dChoiceModel(K, rho, d):
     ddpp = rmf.DDPP()
@@ -9,13 +14,13 @@ def dChoiceModel(K, rho, d):
     def e(i):
         l = np.zeros(K)
         l[i] = 1
-        return(l)
+        return l
 
     # We then add the transitions : 
     for i in range(K):
-        if i>=1:
-            ddpp.add_transition(e(i),eval('lambda x: {}*(x[{}]**{} - x[{}]**{} )'.format(rho,i-1,d,i,d) ))
-        if i<K-1:
+        if i >= 1:
+            ddpp.add_transition(e(i),eval('lambda x: {}*(x[{}]**{} - x[{}]**{} )'.format(rho, i-1, d, i, d)))
+        if i < K-1:
             ddpp.add_transition(-e(i),eval('lambda x: (x[{}] - x[{}])'.format(i,i+1) ))
     ddpp.add_transition(e(0), lambda x : eval('{}*(1-x[0]**{})'.format(rho,d)))
     ddpp.add_transition(-e(K-1), lambda x : x[K-1])
@@ -23,13 +28,17 @@ def dChoiceModel(K, rho, d):
     return ddpp
 
 def generate_data():
+    """
+    Generate all data and store them in a pickle file
+    (to be used one times when the test is initialized)
+    """
     data = dict([])
-    for rho in [0.6,0.7,0.8,0.9]:
-        for d in [2,3]:
-            for K in [5,9,15,20]:
-                for order in ([1, 2] if K<=5 else [1]):
+    for rho in [0.6, 0.7, 0.8, 0.9]:
+        for d in [2, 3]:
+            for K in [5, 9, 15, 20]:
+                for order in ([1, 2] if K <= 5 else [1]):
                     ddpp = dChoiceModel(K, rho, d)
-                    data[(K,rho,d,order)] = ddpp.meanFieldExpansionSteadyState(order=order)
+                    data[(K, rho, d, order)] = ddpp.meanFieldExpansionSteadyState(order=order)
     with open('output_tests/d_choice.pickle', 'wb') as f:
         # Pickle the 'data' dictionary using the highest protocol available.
         pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
@@ -39,7 +48,7 @@ def approximately_equal(new_data, old_data):
     (doc to be written)
     """
     absolute_difference = 0
-    for i in [0, 1] if len(new_data)==3 else [0,1,2]:
+    for i in [0, 1] if len(new_data) == 3 else [0, 1, 2]:
         new = np.array(new_data[i])
         old = np.array(old_data[i])
         absolute_difference += np.sum(np.abs(new-old))
@@ -54,9 +63,9 @@ def test_two_choice():
             (K,rho,d,order) = key
             print(key)
             ddpp = dChoiceModel(K, rho, d)
-            new_data =  ddpp.meanFieldExpansionSteadyState(order=order)
+            new_data = ddpp.meanFieldExpansionSteadyState(order=order)
             test_data = data[key]
-            assert(approximately_equal(new_data, test_data) <= 1e-8)
+            assert approximately_equal(new_data, test_data) <= 1e-8
 
 #generate_data()
 #test_two_choice()
